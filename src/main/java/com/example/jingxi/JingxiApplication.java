5261995d88ba67e8bff912591868108b1f1296bd
ctc <1171332103@qq.com>
2018-05-16 12:11:06 +0800

    first commit

---
+++ src/main/java/com/example/jingxi/JingxiApplication.java
@@ -0,0 +1,12 @@
+package com.example.jingxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingxiApplication.class, args);
+	}
+}
